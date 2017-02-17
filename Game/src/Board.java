e822f97f7534b52dadf9403e652524b7aa59f448
Manuel Pereira <ee12314@fe.up.pt>
2017-02-17 11:57:53 +0000

    Mapa feito

--- Game/src/Board.java
+++ Game/src/Board.java
@@ -1,9 +1,32 @@
-
 public class Board {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	char [][] board = { {'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'},
+		{'X', 'H', ' ', ' ', 'I', ' ', 'X', ' ', 'G', 'X'},
+		{'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+		{'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X'},
+		{'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X'},
+		{'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+		{'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'},
+		{'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X'},
+		{'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X'},
+		{'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'}};
 
+
+
+
+	public static void main(String[] args) {
+	
+		Board board = new Board ();
+	
+		for (int i=0; i<board.board.length; i++){
+			for (int j=0; j<board.board[0].length; j++){
+				System.out.print(board.board[i][j]);
+				System.out.print(' ');
+			}
+			System.out.println();
+		}
+	
+	
 	}
 
 }

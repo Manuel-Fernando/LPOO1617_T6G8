7af72b3fef133af24f0daa9652240cc4ccff865b
Utilizador <Utilizador@193.137.144.27>
2017-03-24 14:35:40 +0000

    Final

--- PL_First/src/dkeep/logic/Entidade.java
+++ PL_First/src/dkeep/logic/Entidade.java
@@ -1,24 +1,52 @@
 package dkeep.logic;
 
+
+/**
+ * Classe que guarda as várias personagens do jogo
+ * @author Carolina e Manuel
+ *
+ */
 public class Entidade {
 
 	char letter;
 	int []pos = new int[2];
 	
+	/**
+	 * Método que movimenta a personagem
+	 * @param dir direção do movimento (a, s, d, ou w)
+	 */
 	protected void Movimento(char dir){}
 	
+	/**
+	 * Método que retorna a posição em x da personagem
+	 * @return inteiro com a posição em x
+	 */
 	protected int getX(){
 		return pos[0];
 	}
 	
+	
+	/**
+	 * Método que retorna a posição em y da personagem
+	 * @return inteiro com a posição em y 
+	 */
 	protected int getY(){
 		return pos[1];
 	}
 	
+	
+	/**
+	 * Método que retorna a letra associada a cada personagem
+	 * @return char com a letra de cada personagem
+	 */
 	protected char getLetter(){
 		return letter;
 	}
 	
+	/**
+	 * Método que atribui uma letra a uma personagem
+	 * @param l char com a letra a atribuir
+	 */
 	protected void setLetter(char l){
 		letter = l;
 	}
--- PL_First/src/dkeep/logic/Heroi.java
+++ PL_First/src/dkeep/logic/Heroi.java
@@ -14,13 +14,13 @@ public class Heroi extends Entidade{
 	
 	private void direcao (int posinX, int posinY, Map m, int mapLevel ){
 		if ((m.searchElement(posinX, posinY) != 'X') && ((m.searchElement(posinX, posinY) != 'I') || herowithkey)){
-			if (mapLevel == 1){direcaoMapLevel1(posinX, posinY, m);} 
-			else if (mapLevel == 2) {direcaoMapLevel2(posinX, posinY, m);}
+			if (mapLevel == 1){int []posss = {posinX, posinY}; direcaoMapLevel1(posss, m);} 
+			else if (mapLevel == 2) {int []posss = {posinX, posinY}; direcaoMapLevel2(posss, m);}
 		}
 	}
 	 
-	public void direcaoMapLevel1(int posinX, int posinY, Map m){					
-			if (m.searchElement(posinX, posinY) == 'k') {
+	public void direcaoMapLevel1(int []posinXY, Map m){					
+			if (m.searchElement(posinXY[0], posinXY[1]) == 'k') {
 				for (int i=0; i<m.board.length; i++){
 					for (int j=0; j<m.board[0].length; j++){
 						if(m.searchElement(i, j)=='I' && j==0){ 
@@ -34,46 +34,46 @@ public class Heroi extends Entidade{
 			}
 			
 			m.writeElement(pos, ' ');
-			pos[0]=posinX;
-			pos[1]=posinY;
+			pos[0]=posinXY[0];
+			pos[1]=posinXY[1];
 			m.writeElement(pos, letter);
 		
 	}
 
-	public void direcaoMapLevel2(int posinX, int posinY, Map m){
-		if (m.searchElement(posinX, posinY) == '+') {
+	public void direcaoMapLevel2(int []posinXY, Map m){
+		if (m.searchElement(posinXY[0], posinXY[1]) == '+') {
 			herowithclub = true;
 			if(herowithkey){letter = 'K';
 			}else
 				letter = 'A';
 		}
-		if (m.searchElement(posinX, posinY) == 'k') {
+		if (m.searchElement(posinXY[0], posinXY[1]) == 'k') {
 			letter = 'K';
 			herowithkey=true;
 		}
 
-		if (posinX == 0 || posinY == 0 || posinX == m.board.length-1|| posinY == m.board[0].length-1){
-			openDoorLevel2 (m, posinX, posinY);
-		} else if (m.searchElement(posinX, posinY) != 'I') { 
+		if (posinXY[0] == 0 || posinXY[1] == 0 || posinXY[0] == m.board.length-1|| posinXY[1] == m.board[0].length-1){
+			openDoorLevel2 (m, posinXY);
+		} else if (m.searchElement(posinXY[0], posinXY[1]) != 'I') { 
 			m.writeElement(pos, ' ');
-			pos[0]=posinX;
-			pos[1]=posinY;
+			pos[0]=posinXY[0];
+			pos[1]=posinXY[1];
 			m.writeElement(pos, letter);
 		}
 		
 	}
 	
-	public void openDoorLevel2 (Map m, int posinX, int posinY){
+	public void openDoorLevel2 (Map m, int []posinXY){
 		if (count == 1){
 			m.writeElement(pos, ' ');
-			pos[0]=posinX;
-			pos[1]=posinY;
+			pos[0]=posinXY[0];
+			pos[1]=posinXY[1];
 			m.writeElement(pos, letter);
 		} else {
 			count=1;
 			for (int i=0; i<m.board.length; i++){
 				for (int j=0; j<m.board[0].length; j++){
-					if(m.searchElement(i, j)=='I' && i==posinX && j==posinY){
+					if(m.searchElement(i, j)=='I' && i==posinXY[0] && j==posinXY[1]){
 						int []posij = {i,j};
 						m.writeElement(posij, 'S');
 					}
